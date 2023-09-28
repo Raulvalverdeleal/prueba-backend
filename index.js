e37b6b51042914dc2160189e585a52401c21b446
raul <raulvalverdeleal@alumnos.cei.es>
2023-09-28 14:55:50 +0200

    backend finalizado

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+let puerto = process.env.PORT || 4000;
+const servidor = express();
+
+servidor.get("/",(peticion,respuesta) => {
+	let [r,g,b] = [0,0,0].map(() => Math.floor(Math.random() * 256));
+	respuesta.json({r,g,b});
+})
+
+servidor.use((peticion,respuesta) => {
+	respuesta.status(404);
+	respuesta.send("not found...")
+})
+
+
+servidor.listen(puerto);
