bde9e1401e73c501481e8ca94c138cc2b57ff35d
raul <raulvalverdeleal@alumnos.cei.es>
2023-09-29 13:02:56 +0200

    creación ficheros estáticos

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 let puerto = process.env.PORT || 4000;
 const servidor = express();
 
+servidor.use("/prueba",express.static("./estaticos"))
+
 servidor.get("/",(peticion,respuesta) => {
 	let [r,g,b] = [0,0,0].map(() => Math.floor(Math.random() * 256));
 	respuesta.json({r,g,b});
